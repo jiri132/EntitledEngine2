0dd4a19e9928a311eebe6dfad8d0e9ec54ca85c9
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-06-12 23:52:40 +0200

    added sprite colors

--- Engine/Core/Shapes/Sprite.cs
+++ Engine/Core/Shapes/Sprite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,6 @@ namespace EntitledEngine2.Engine.Core.Shapes
 	public abstract class Sprite
 	{
 		public abstract Vector2[] GetDrawingPoints();
+		public abstract Color GetColor();
 	}
 }
