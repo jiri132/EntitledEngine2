938b68186126281cd50dbc96f1abf8db3e8b7ae8
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-07-04 23:09:26 +0200

    Added a debugger

---
+++ Engine/Core/Debug.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitledEngine2.Engine.Core
+{
+    class Debug
+    {
+        public static void Log(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"[LOG] - {s}");
+            Console.ResetColor();
+        }
+        public static void Warning(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[WARNING] - {s}");
+            Console.ResetColor();
+        }
+
+        public static void Error(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[ERROR] - {s}");
+            Console.ResetColor();
+        }
+        public static void CustomLog(string s, ConsoleColor c)
+        {
+            Console.ForegroundColor = c;
+
+            Console.WriteLine($"[LOG] - {s}");
+            Console.ResetColor();
+        }
+    }
+}
