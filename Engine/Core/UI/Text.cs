c92b63e9bec0d00076c53373b25cf178500dde09
jiri132 <76393975+jiri132@users.noreply.github.com>
2022-07-12 13:19:06 +0200

    Started Development on UI

---
+++ Engine/Core/UI/Text.cs
@@ -0,0 +1,17 @@
+﻿using EntitledEngine2.Engine.Core.Vec2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitledEngine2.Engine.Core.UI
+{
+    public class Text : UI
+    {
+        public override Vector2 Position()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Engine/Core/UI/UI.cs
@@ -0,0 +1,18 @@
+﻿using EntitledEngine2.Engine.Core.Vec2;
+using EntitledEngine2.Engine.Core.Maths;
+
+namespace EntitledEngine2.Engine.Core.UI
+{
+    public abstract class UI
+    {
+        public Vector2 clampedPosition => CalculateClampedPosition();
+        public abstract Vector2 Position();
+        private Vector2 CalculateClampedPosition()
+        {
+            Vector2 position = Position();
+
+            return null;
+
+        }
+    }
+}
